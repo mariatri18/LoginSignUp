0d2dbfd636ffc35230adb1e064f635784e8460d5
mariatri18 <46164228+mariatri18@users.noreply.github.com>
2019-09-24 17:35:23 +0300

    Add files via upload

---
+++ Login_form.java
@@ -0,0 +1,23 @@
+package com.example.loginsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
+        getSupportActionBar().setTitle("Login Form");
+    }
+
+    public void btn_signUpForm(View view) {
+
+        startActivity(new Intent(getApplicationContext(),SignUp_Form.class));
+
+    }
+}
